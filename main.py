96b1bb8dfa5616d62d711a150a4a8ba9884de6b3
MikhailBasiy <mikhail.basiy@gmail.com>
2025-04-02 14:55:11 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from icecream import ic
+
+from get_site_prices import get_site_prices
+from get_db_prices import get_db_prices
+
+
+def compare_prices(joined_data: pd.DataFrame) -> pd.DataFrame:
+    return
+
+
+def join_prices(
+        site_prices: pd.DataFrame, 
+        db_prices: pd.DataFrame
+) -> pd.DataFrame:
+    return
+
+
+if __name__ == "__main__":
+    site_prices: pd.DataFrame = get_site_prices()
+    site_prices.to_excel("site_prices.xlsx", index=False, engine="xlsxwriter")
+    # db_prices: pd.DataFrame = get_db_prices()
+    # joined_data = join_prices(site_prices, db_prices)
+    # compared_prices = compare_prices(joined_data)
+    # compared_prices = pd.to_excel("compared_prices", index=False, engine="xlsxwriter")
