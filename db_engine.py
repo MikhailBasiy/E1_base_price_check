c96eb3a6225209f1dc24b5a5ecedfa6350c3511c
MikhailBasiy <mikhail.basiy@gmail.com>
2025-05-06 10:53:41 +0300

    fix loading system envs

--- db_engine.py
+++ db_engine.py
@@ -6,7 +6,7 @@ from sqlalchemy.engine.base import Engine
 
 
 def get_engine() -> Engine:
-    load_dotenv()
+    load_dotenv(override=True)
     DB_HOST = getenv("DB_HOST")
     DB_NAME = getenv("DB_NAME")
     DB_USERNAME = getenv("DB_USERNAME")
@@ -16,4 +16,5 @@ def get_engine() -> Engine:
         f"mssql+pyodbc://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/"
         f"{DB_NAME}?driver=ODBC+Driver+17+for+SQL+Server"
     )
+
     return engine
